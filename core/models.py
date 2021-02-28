f4ab797242fbf6286986bf82c5d64bc8a982ed1d
Paulo Freitas <paulogeo5@gmail.com>
2021-02-28 16:20:08 -0300

    Adiciona metadados

--- core/models.py
+++ core/models.py
@@ -1,8 +1,17 @@
 from django.contrib.gis.db import models
 
 class Escola(models.Model):
+    """
+    Modelo que representa uma escola
+    """
+
     nome = models.CharField(max_length=200)
     geom = models.PointField()
 
+    class Meta:
+        ordering = ['nome']
+
     def __str__(self):
         return self.nome
+    
+
